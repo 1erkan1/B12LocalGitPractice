16d01ec88fbab844d47f1fcbf549b12437713451
Erkan <youremail@yourdomain.com>
2023-11-03 21:04:47 +0000

    Test

--- src/Test1/TestGit1.java
+++ src/Test1/TestGit1.java
@@ -7,7 +7,6 @@ public class TestGit1 {
         System.out.println("Test1");
         System.out.println("Test2");
         System.out.println("Test3");
-        System.out.println("Test4");
         System.out.println("Edited code");
     }
 }
