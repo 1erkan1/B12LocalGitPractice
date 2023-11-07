6da9cec4a3f12c26290459f85a0727e8534b7ab4
Erkan <youremail@yourdomain.com>
2023-11-07 18:03:55 +0000

    Erkan file created, test updated

--- src/Test1/TestGit1.java
+++ src/Test1/TestGit1.java
@@ -5,7 +5,7 @@ public class TestGit1 {
     public static void main(String[] args) {
         System.out.println("GIT");
         System.out.println("Test1");
-        System.out.println("Test2");
+        System.out.println("MyPractices");
         System.out.println("Test3");
         System.out.println("Edited code");
     }
